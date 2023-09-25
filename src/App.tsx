f69707cb511ba40498390172ad2b2a59f250a4c4
Akshay Kanabar <Akshay.Kanabar@trafficmanagement.com>
2023-09-25 22:19:24 +0530

    updates on directions

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import "./App.css";
-import {
-  GoogleMap,
-  DirectionsRenderer,
-  PolylineF,
-  MarkerF,
-} from "@react-google-maps/api";
+import { GoogleMap, DirectionsRenderer, MarkerF } from "@react-google-maps/api";
 import useMap from "./hooks/useMap";
 import { CONTAINER_STYLE, MAP_STARTING_CENTER } from "./utils/constants";
 
@@ -15,31 +10,43 @@ function App() {
     onUnmount,
     originRef,
     destinationRef,
+    origin,
+    destination,
     getDirections,
     directions,
-    snapToRoads,
-    snappedCoordinates,
-    snappedPoints,
-    setSnappedPoints,
+    distance,
+    duration,
+    rotation,
   } = useMap();
+
+  const directionsOptions = {
+    suppressMarkers: true,
+    polylineOptions: {
+      strokeColor: "red",
+      strokeWeight: 6,
+    },
+  };
 
   return (
     <>
       {isLoaded ? (
         <div id="container">
           <div className="action_bar">
+            <label style={{ color: "white" }}>Start Point</label>
             <input
               ref={originRef}
-              defaultValue="1476 Canyon Crest Drive, Corona, CA"
-              placeholder="Start Poin"
+              defaultValue="700 E Lake Drive, Orange CA"
+              placeholder="Start Point"
             />
+            <label style={{ color: "white" }}>End Point</label>
             <input
               ref={destinationRef}
-              defaultValue="700 E Lake Drive, Orange CA"
+              defaultValue="1476 Canyon Crest Drive, Corona, CA"
               placeholder="End Point"
             />
             <button onClick={getDirections}>Get Directions</button>
-            <button onClick={snapToRoads}>Snap To Roads</button>
+            <label style={{ color: "white" }}>Distance : {distance}</label>
+            <label style={{ color: "white" }}> Duration : {duration}</label>
           </div>
           <div className="map_container">
             <GoogleMap
@@ -50,19 +57,31 @@ function App() {
               mapContainerStyle={CONTAINER_STYLE}
               onClick={(e) => {
                 console.log(e.latLng?.toUrlValue());
-                if (e.latLng) {
-                  console.log(snappedPoints);
-                  snappedPoints.push(e.latLng?.toUrlValue());
-                  console.log(snappedPoints);
-                  setSnappedPoints(snappedPoints);
-                  setTimeout(() => {
-                    snapToRoads();
-                  }, 0);
-                }
+                // if (e.latLng) {
+                //   snappedPoints.push(e.latLng?.toUrlValue());
+                //   setSnappedPoints(snappedPoints);
+                // }
               }}
             >
-              {directions && <DirectionsRenderer directions={directions} />}
-              {snappedCoordinates && (
+              {origin && (
+                <MarkerF
+                  icon={{
+                    url: "../src/assets/Navigation.svg",
+                    scaledSize: new google.maps.Size(32, 32),
+                    anchor: new google.maps.Point(16, 16),
+                    rotation: rotation,
+                  }}
+                  position={origin}
+                />
+              )}
+              {destination && <MarkerF position={destination} />}
+              {directions && (
+                <DirectionsRenderer
+                  options={directionsOptions}
+                  directions={directions}
+                />
+              )}
+              {/* {snappedCoordinates && (
                 <PolylineF
                   path={snappedCoordinates}
                   options={{
@@ -71,7 +90,7 @@ function App() {
                     strokeWeight: 3,
                   }}
                 />
-              )}
+              )} */}
               {/* <>
                 {Object.values(snappedCoordinates).map((loc, index) => (
                   <MarkerF markerStyle key={index} position={loc} />
--- src/hooks/useMap.ts
+++ src/hooks/useMap.ts
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useJsApiLoader, Libraries } from "@react-google-maps/api";
 import { MAP_STARTING_CENTER } from "../utils/constants";
 
@@ -7,20 +7,27 @@ const LIBRARIES: Libraries | undefined = ["places"];
 export default function useMap() {
   const originRef = React.useRef<HTMLInputElement>(null);
   const destinationRef = React.useRef<HTMLInputElement>(null);
+
   const [directions, setDirections] =
     React.useState<google.maps.DirectionsResult>();
   const [snappedPoints, setSnappedPoints] = React.useState<any[]>([]);
   const [snappedCoordinates, setSnappedCoordinates] = React.useState<any[]>([]);
-
+  const [distance, setDistance] = React.useState("");
+  const [duration, setDuration] = React.useState("");
+  const [origin, setOrigin] = React.useState<
+    google.maps.LatLng | google.maps.LatLngLiteral
+  >();
+  const [count, setCount] = React.useState<number>(0);
+  const [destination, setDestination] = React.useState<any>(null);
   const GOOGLE_MAPS_KEY = import.meta.env.VITE_GOOGLE_MAPS_KEY;
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_KEY,
     libraries: LIBRARIES,
   });
-
   const [map, setMap] = React.useState<google.maps.Map | null>(null);
 
+  const [rotation, setRotation] = React.useState(0);
   const onLoad = React.useCallback(function callback(map: google.maps.Map) {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
     const bounds = new window.google.maps.LatLngBounds(MAP_STARTING_CENTER);
@@ -42,31 +49,97 @@ export default function useMap() {
       destination: destinationRef.current.value,
       travelMode: google.maps.TravelMode.DRIVING,
     });
+    setOrigin({
+      lat: results.routes[0].legs[0].start_location.lat(),
+      lng: results.routes[0].legs[0].start_location.lng(),
+    });
+    setDestination({
+      lat: results.routes[0].legs[0].end_location.lat(),
+      lng: results.routes[0].legs[0].end_location.lng(),
+    });
+    setDistanceAndDuration(results);
 
-    // var pathValues = [];
-    // for (var i = 0; i < results.routes.length; i++) {
-    //   for (var j = 0; j < results.routes[i].legs[0].steps.length; j++) {
-    //     pathValues.push(
-    //       results.routes[i].legs[0].steps[j].start_location.toUrlValue()
-    //     );
-    //     pathValues.push(
-    //       results.routes[i].legs[0].steps[j].end_location.toUrlValue()
-    //     );
-    //   }
-    // }
-    //setSnappedPoints(pathValues);
-    setDirections(results);
+    var pathValues = [];
+    for (var i = 0; i < results.routes.length; i++) {
+      for (var j = 0; j < results.routes[i].legs[0].steps.length; j++) {
+        pathValues.push(
+          results.routes[i].legs[0].steps[j].start_location.toUrlValue()
+        );
+        pathValues.push(
+          results.routes[i].legs[0].steps[j].end_location.toUrlValue()
+        );
+      }
+    }
+    setSnappedPoints(pathValues);
   }
 
-  async function snapToRoads() {
-    // const snappedPoints = [
-    //   { lat: 37.772, lng: -122.214 },
-    //   { lat: 21.291, lng: -157.821 },
-    //   { lat: -18.142, lng: 178.431 },
-    //   { lat: -27.467, lng: 153.027 },
-    // ];
-    if (snappedPoints.length < 2) return;
+  const interval = setInterval(async () => {
+    if (snappedPoints.length > 0 && snappedPoints[count]) {
+      await getUpdateDirections(snappedPoints[count]);
+    }
+    clearInterval(interval);
+  }, 10000);
 
+  useMemo(() => {
+    if (count < snappedPoints.length) {
+      interval;
+    }
+  }, [count]);
+
+  async function getUpdateDirections(newLocationRef: any) {
+    if (!destinationRef.current) return;
+    const directionsService = new google.maps.DirectionsService();
+    const results = await directionsService.route({
+      origin: newLocationRef,
+      destination: destinationRef.current.value,
+      travelMode: google.maps.TravelMode.DRIVING,
+    });
+    setDistanceAndDuration(results);
+    setDirections(results);
+    if (count < snappedPoints.length) setCount(count + 1);
+    setTimeout(() => {
+      updateMarkerPosition({
+        lat: Number(snappedPoints[count].split(",")[0]),
+        lng: Number(snappedPoints[count].split(",")[1]),
+      });
+    });
+  }
+
+  async function setDistanceAndDuration(results: google.maps.DirectionsResult) {
+    if (results.routes[0].legs[0] && results.routes[0].legs[0].distance) {
+      setDistance(results.routes[0].legs[0].distance.text);
+    } else {
+      setDistance("");
+    }
+    if (results.routes[0].legs[0] && results.routes[0].legs[0].duration) {
+      setDuration(results.routes[0].legs[0].duration.text ?? 0);
+    } else {
+      setDuration("");
+    }
+  }
+
+  const updateMarkerPosition = (newCoordinates: any) => {
+    // Calculate rotation angle based on movement direction
+    const newRotation = calculateRotationAngle(newCoordinates);
+    console.log(newRotation);
+    setOrigin(newCoordinates);
+    setRotation(newRotation);
+  };
+
+  const calculateRotationAngle = (newCoordinates: any) => {
+    const prevLat = Number(origin?.lat);
+    const prevLng = Number(origin?.lng);
+    const { lat: newLat, lng: newLng } = newCoordinates;
+
+    const deltaY = newLat - prevLat;
+    const deltaX = newLng - prevLng;
+
+    const angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
+
+    return angle;
+  };
+
+  async function snapToRoads() {
     const response = await fetch(
       `https://roads.googleapis.com/v1/snapToRoads?path=${encodeURIComponent(
         snappedPoints.join("|")
@@ -74,7 +147,6 @@ export default function useMap() {
     );
 
     const data = await response.json();
-
     // Extract the snapped points from the response
     const snappedCoordinates = data.snappedPoints.map((point: any) => ({
       lat: point.location.latitude,
@@ -91,10 +163,18 @@ export default function useMap() {
     getDirections,
     originRef,
     destinationRef,
+    origin,
+    destination,
     directions,
     snappedPoints,
     setSnappedPoints,
     snapToRoads,
+    distance,
+    duration,
+    getUpdateDirections,
     snappedCoordinates,
+    count,
+    setCount,
+    rotation,
   };
 }
